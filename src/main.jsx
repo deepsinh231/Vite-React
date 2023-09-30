0952419426b4a1b3a373795f6f5ffeed5f527a2d
deepsinh231 <solankideepsinh231@gmail.com>
2023-09-30 18:16:41 +0530

    how to React Working

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 
+
+function Myapp() {
+  return (
+    <h1>Hello</h1>
+  )
+};
+const anotherUser = "chai aur react";
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit google</a>
+);
+const reactElement = React.createElement(
+  'a',
+  { href: 'https://google.com', target: '_blank' },
+  'click me to visit google',
+  anotherElement
+);
 ReactDOM.createRoot(document.getElementById('Deepsinh231')).render(
-  <App />
+  <>
+    {reactElement}
+    {Myapp()}
+  </>
+
 )
+// how to React Working
