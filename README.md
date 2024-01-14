ffec7e3cba24437c03b506c35b2991d47eb297dc
Deepsinh231 <deepsinh231@gmail.com>
2024-01-14 19:09:42 +0530

    ns

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+1.ScrollRestoration <br/>
+2.Outlet <br/>
+3.NavLink //isActive <br/>
+4.useParams  <br/>
+4.useLoaderData  //loader  <br/>
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, ScrollRestoration } from 'react-router-dom'
 import { Header, Footer } from './Components'
 
 function Layout() {
     return (
         <>
+
             <Header />
             <Outlet />
             <Footer />
+            <ScrollRestoration
+                getKey={(location, matches) => {
+                    return location.pathname;
+                }}
+            />
         </>
     )
 }
