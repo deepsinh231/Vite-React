fe46715ddf2eb621d91f4de5608d360e08cdb000
Deepsinh231 <deepsinh231@gmail.com>
2024-01-18 10:15:07 +0530

    New

--- src/componet/Llisttodo.jsx
+++ src/componet/Llisttodo.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { removetodo } from '../allfunction/minions'
+import { GoX } from "react-icons/go"
 
 export default function Llisttodo() {
+    const [showModal, setShowModal] = useState(false);
     const todos = useSelector(start => start.todos)
     const dispatch = useDispatch()
+    const [input, setinput] = useState('')
+    const addTodoHandler = (e) => {
+        e.preventDefault()
+        dispatch(addtodo({ id: 1, Text: "hellow word" }))
+        setinput("")
+    }
     return (
         <>
             <div>Todos</div>
@@ -25,7 +33,7 @@ export default function Llisttodo() {
                                     xmlns="http://www.w3.org/2000/svg"
                                     fill="none"
                                     viewBox="0 0 24 24"
-                                    strokeWidth={1.5}
+                                    width={1.5}
                                     stroke="currentColor"
                                     className="w-6 h-6"
                                 >
@@ -36,18 +44,68 @@ export default function Llisttodo() {
                                     />
                                 </svg>
                             </button>
-                            <div className="m-5"></div>
-                            <button
-                                onClick={() => dispatch(removetodo(todo.id))}
+                            {/* <div className="m-5"></div> */}
+                            {/* <button
+                                onClick={() => setShowModal(true)}
                                 className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
                             >
-                                <svg stroke="currentColor" fill="currentColor" stroke-width={1.5} viewBox="0 0 1024 1024" className='w-6 h-6'
+                                <svg stroke="currentColor" fill="currentColor" width={1.5} viewBox="0 0 1024 1024" className='w-6 h-6'
                                     xmlns="http://www.w3.org/2000/svg"><path d="M880 836H144c-17.7 0-32 14.3-32 32v36c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-36c0-17.7-14.3-32-32-32zm-622.3-84c2 0 4-.2 6-.5L431.9 722c2-.4 3.9-1.3 5.3-2.8l423.9-423.9a9.96 9.96 0 0 0 0-14.1L694.9 114.9c-1.9-1.9-4.4-2.9-7.1-2.9s-5.2 1-7.1 2.9L256.8 538.8c-1.5 1.5-2.4 3.3-2.8 5.3l-29.5 168.2a33.5 33.5 0 0 0 9.4 29.8c6.6 6.4 14.9 9.9 23.8 9.9z"></path></svg>
-                            </button>
+                            </button> */}
                         </div>
                     </li>
                 ))}
             </ul>
+            {showModal ? (
+                <>
+                    <div
+                        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                    >
+                        <div className="relative my-6 mx-auto w-1/2">
+                            {/*content*/}
+                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                {/*header*/}
+                                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                                    <h3 className="text-3xl font-semibold text-center">
+                                        Edit Todos
+                                    </h3>
+                                    <button
+                                        className="p-1 ml-auto bg-transparent border-0 text-black  float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                                        <GoX />
+                                    </button>
+                                </div>
+                                {/*body*/}
+                                <form onSubmit={addTodoHandler} className="text-center">
+                                    <div className="relative p-6 flex-auto">
+                                        <div className="my-4 text-blueGray-500 text-lg leading-relaxed">
+                                            <input
+                                                type="text"
+                                                className="bg-gray-800 w-full rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                                placeholder="Enter a Todo..."
+                                                value={input}
+                                                onChange={(e) => setinput(e.target.value)}
+                                            />
+                                        </div>
+                                    </div>
+                                    {/*footer*/}
+                                    <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                                        <button
+                                            className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                            type="submit"
+                                            onClick={() => setShowModal(false)}
+                                        >
+                                            Add Todo
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                </>
+            ) : null}
         </>
     )
 }
